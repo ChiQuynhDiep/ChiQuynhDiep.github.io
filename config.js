7779632434181d5107087a59f14aaf1c6973ffcd
Minh Trọng Nguyễn <minhnguyen@adnovum.vn>
2022-10-20 18:49:24 +0700

    init

---
+++ config.js
@@ -0,0 +1,36 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '哈囉 ',
+    introDesc: `今天是個蠻特別的日子。。。我有事想跟你講的，你想不想聽🤭`,
+    btnIntro: 'OK',
+    title: '今天是你的生日，你猜猜我在想什麼的啊😎',
+    desc: 'Uhm，讓我知道你的答案。^_^> ',
+    btnYes: 'uhm, 我猜猜 ',
+    btnNo: '不知道啦，猜不出來',
+    question:'也許是',
+    reasonPlaceholder: '在這兒輸入你的答案...',
+    btnReply: 'Send',
+    reply: '我只想跟你說一句話就是 祝你生日快樂，趕早達到自己的目標，每天都愉快、笑著🤜🤛。27歲是個不小也不大的年齡，加油👍',
+    mess: ' Hôm nay, đường phố bán bông\tHôm nay, đường phố bán bông\n\
+    Rộn ràng ……, hoa hồng trên tay\tRộn ràng ……, hoa hồng trên tay\n\
+    Mình đi, cũng thấy hay hay\tRộn ràng ……, hoa hồng trên tay\n\
+    Dặn lòng tự hỏi hôm nay ngày gì\tRộn ràng ……, hoa hồng trên tay?\
+    ',
+    poem1:'    Hôm nay, đường phố bán bông\n\
+    Rộn ràng ai cũng đóa hồng trên tay\n\
+    Mình đi, bỗng thấy hay hay\n\
+    Dặn lòng tự hỏi hôm nay ngày gì?\n\
+    Thì ra ngày lễ chứ chi\n\
+    Nhìn qua ghế chị mà đâu mất rồi\n\
+    ',
+    poem2:'    Ngồi đây cầm bút vẽ vời\n\
+    Viết thơ chúc chị những lời đẹp tươi \n\
+    Chúc luôn vui vẻ nụ cười\n\
+    Ngày càng thêm trẻ rạng ngời như hoa\n\
+    Chúc cho cuộc sống thuận hòa\n\
+    Ngập tràn năng lượng đong đầy yêu thương.\n\
+    ',
+    messTitle: "Gửi Chị Diệp",
+    messDesc: 'Chúc chị Diệp 20/10 thật vui vẻ 🥳',
+    btnAccept: 'Ok lun',
+}
